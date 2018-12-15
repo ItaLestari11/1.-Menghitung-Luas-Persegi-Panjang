42a1f1f67ef6f58fa4e41ed1e5096747430048d2
ItaLestari11 <45660967+ItaLestari11@users.noreply.github.com>
2018-12-15 15:03:55 +0700

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+int p,l,luas;
+
+cout<<"masukan nilai panjang:";
+cin>>p;
+cout<<"masukan nilai lebar:";
+cin>>l;
+luas=p*l;
+cout<<"luas persegi panjang adalah"<<luas;
+return 0;
+
+}
